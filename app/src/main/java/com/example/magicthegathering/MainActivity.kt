6da6077615404543508e4279950e982d979edbb5
Alex Doub <alexdoub1990@gmail.com>
2020-03-09 21:28:14 -0700

    data binding for activity

--- app/src/main/java/com/example/magicthegathering/MainActivity.kt
+++ app/src/main/java/com/example/magicthegathering/MainActivity.kt
@@ -2,20 +2,23 @@ package com.example.magicthegathering
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.magicthegathering.ui.cards.list.ListCardsFragment
-import com.squareup.picasso.LruCache
-import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
+    //    lateinit var binding: MainActivityBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main_activity)
+        //or
+//        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, ListCardsFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, ListCardsFragment.newInstance())
+                .commitNow()
         }
     }
 }
